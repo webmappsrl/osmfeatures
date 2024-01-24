593cc2572ee255c8720748aa4e58b2457940f129
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-24 15:20:20 +0100

    feat: swagger 1st version

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -197,8 +197,7 @@ return [
                     'name' => 'Authorization', // The name of the header or query parameter to be used.
                     'in' => 'header', // The location of the API key. Valid values are "query" or "header".
                 ],
-                */
-            ],
+                */],
             'security' => [
                 /*
                  * Examples of Securities
@@ -211,8 +210,7 @@ return [
                     ],
 
                     'passport' => []
-                    */
-                ],
+                    */],
             ],
         ],
 
