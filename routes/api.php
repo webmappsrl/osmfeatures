7c3ee11f7db1342c5a0424b3d745bee7f8fb6d94
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-24 15:00:51 +0100

    feat: implemented poi list api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PoiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PoiController;
 
 /*
 |--------------------------------------------------------------------------
