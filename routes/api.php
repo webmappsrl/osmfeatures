cb5bfd2ef90ed35580b15c0363ba1b2df3065d56
gemanzo <gemanzo@users.noreply.github.com>
2024-01-24 14:01:22 +0000

    Check and fix styling

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\PoiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PoiController;
 
 /*
 |--------------------------------------------------------------------------
