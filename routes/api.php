8614d408c6c3f78360a68aa8b1b85febc452e912
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-29 10:42:40 +0100

    feat: enhanced API documentation

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/features/pois/list', [PoiController::class, 'list']);
-Route::get('/features/pois/{id}', [PoiController::class, 'show']);
+Route::prefix('v1')->name('api.v1.')->group(function () {
+    Route::get('/features/pois/list', [PoiController::class, 'list']);
+    Route::get('/features/pois/{id}', [PoiController::class, 'show']);
+});
