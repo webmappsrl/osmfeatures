d4a3e90c8afac6170d245970527e94c8a82b7c5c
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-04 16:18:02 +0100

    feat: added filters for wikis in nova resources admin areas poles and pois

---
+++ app/Nova/Filters/WikiPediaFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Laravel\Nova\Filters\BooleanFilter;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class WikiPediaFilter extends BooleanFilter
+{
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(NovaRequest $request, $query, $value)
+    {
+        if ($value['has_wikipedia']) {
+            return $query->whereRaw("jsonb_exists(cast(tags as jsonb), 'wikipedia')");
+        }
+
+        if ($value['no_wikipedia']) {
+            return $query->whereRaw("NOT jsonb_exists(cast(tags as jsonb), 'wikipedia')");
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function options(NovaRequest $request)
+    {
+        return [
+            'Has wikipedia' => 'has_wikipedia',
+            'No wikipedia' => 'no_wikipedia',
+        ];
+    }
+}
