5ba5c8b9ef4f2c5f8179870bbf06b433920a8a4a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-04 16:16:15 +0100

    feat: created trait for processing osm tags data

---
+++ app/Traits/OsmTagsProcessor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+
+trait OsmTagsProcessor
+{
+    /**
+     * Get the wikidata from tags column if it exists
+     * @return string|null
+     */
+    public function getWikidata(): ?string
+    {
+        $tags = json_decode($this->tags, true);
+
+        return $tags['wikidata'] ?? null;
+    }
+
+    /**
+     * Get the wikimedia commons from tags column if it exists
+     * @return string|null
+     */
+    public function getWikimediaCommons(): ?string
+    {
+        $tags = json_decode($this->tags, true);
+
+        return $tags['wikimedia_commons'] ?? null;
+    }
+
+    /**
+     * Get the wikipedia from tags column if it exists
+     * @return string|null
+     */
+    public function getWikipedia(): ?string
+    {
+        $tags = json_decode($this->tags, true);
+
+        return $tags['wikipedia'] ?? null;
+    }
+}
