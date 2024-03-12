44538ddafd47be98169bcf232988c849b973dd84
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-12 08:46:41 +0100

    feat: created hiking routes lua file

---
+++ storage/osm/lua/hiking_routes.lua
@@ -0,0 +1,163 @@
+local hiking_routes = osm2pgsql.define_table({
+    name = 'hiking_routes',
+    schema = 'public',
+    ids = { type = 'any', type_column = 'osm_type', id_column = 'osm_id' },
+    columns = {
+        {column = 'id', sql_type = 'serial', create_only = true},
+        { column = 'name', type = 'text' },
+        { column = 'cai_scale', type = 'text' },
+        { column = 'osmc_symbol', type = 'text' },
+        { column = 'network', type= 'text' },
+        { column = 'survey_date', type= 'text' },
+        { column = 'roundtrip', type= 'text' },
+        { column = 'symbol', type= 'text' },
+        { column = 'symbol_it', type= 'text' },
+        { column = 'ascent', type= 'text' },
+        { column = 'descent', type= 'text' },
+        { column = 'distance', type= 'text' },
+        { column = 'duration_forward', type= 'text' },
+        { column = 'duration_backward', type= 'text' },
+        { column = 'from', type= 'text' },
+        { column = 'to', type= 'text' },
+        { column = 'rwn_name', type= 'text' },
+        { column = 'ref_REI', type= 'text' },
+        { column = 'maintenance', type= 'text' },
+        { column = 'maintenance_it', type= 'text' },
+        { column = 'operator', type= 'text' },
+        { column = 'state', type= 'text' },
+        { column = 'ref', type= 'text' },
+        { column = 'source', type= 'text' },
+        { column = 'source_ref', type= 'text' },
+        { column = 'note', type= 'text' },
+        { column = 'note_it', type= 'text' },
+        { column = 'old_ref', type= 'text' },
+        { column = 'note_project_page', type= 'text' },
+        { column = 'website', type= 'text' },
+        { column = 'wikimedia_commons', type= 'text' },
+        { column = 'description', type= 'text' },
+        { column = 'description_it', type= 'text' },
+        { column = 'tags', type = 'jsonb'},
+        { column = 'geom', type = 'multilinestring', projection = 4326 },
+    }
+})
+
+local hiking_ways = osm2pgsql.define_table({
+    name = 'hiking_ways',
+    schema = 'public',
+    ids = { type = 'any', type_column = 'osm_type', id_column = 'osm_id' },
+    columns = {
+        {column = 'id', sql_type = 'serial', create_only = true},
+        { column = 'trail_visibility', type='text'},
+        { column = 'sac_scale', type='text'},
+        {column = 'tracktype', type='text'},
+        { column = 'highway', type='text'},
+        { column = 'name', type='text'},
+        { column = 'ref', type='text'},
+        { column = 'access', type='text'},
+        { column = 'incline', type='text'},
+        { column = 'surface', type='text'},
+        { column = 'ford', type='bool'},
+        { column = 'tags', type = 'jsonb' },
+        { column = 'geom', type = 'linestring' },
+        { column = 'rel_refs', type = 'text' }, 
+        { column = 'rel_ids',  type = 'int8' },
+    }
+})
+
+local w2r = {}
+
+function process_hiking_route(object, geom)
+
+    local a = {
+        name = object.tags.name,
+        cai_scale = object.tags['cai:scale'],
+        osmc_symbol = object.tags['osmc:symbol'],
+        network = object.tags.network,
+        survey_date = object.tags['survey:date'],
+        roundtrip = object.tags.roundtrip,
+        symbol = object.tags.symbol,
+        symbol_it = object.tags['symbol:it'],
+        ascent = object.tags.ascent,
+        descent = object.tags.descent,
+        distance = object.tags.distance,
+        duration_forward = object.tags['duration:forward'],
+        duration_backward = object.tags['duration:backward'],
+        from = object.tags.from,
+        to = object.tags.to,
+        rwn_name = object.tags['rwn:name'],
+        ref_REI = object.tags['ref:REI'],
+        maintenance = object.tags.maintenance,
+        maintenance_it = object.tags['maintenance:it'],
+        operator = object.tags.operator,
+        state = object.tags.state,
+        ref = object.tags.ref,
+        source = object.tags.source,
+        source_ref = object.tags['source:ref'],
+        note = object.tags.note,
+        note_it = object.tags['note:it'],
+        old_ref = object.tags['old_ref'],
+        note_project_page = object.tags['note:project_page'],
+        website = object.tags.website,
+        wikimedia_commons = object.tags['wikimedia_commons'],
+        description = object.tags.description,
+        description_it = object.tags['description:it'],
+        tags = object.tags,
+        geom = geom,
+    }
+   for _, member in ipairs(object.members) do
+            if member.type == 'w' then
+                if not w2r[member.ref] then
+                    w2r[member.ref] = {}
+                end
+                w2r[member.ref][object.id] = object.tags.ref
+            end
+        end
+    hiking_routes:insert(a)
+end
+
+function osm2pgsql.process_way(object)
+    if not object.tags.highway then
+        return
+    end
+    local a = {
+        trail_visibility = object.tags['trail_visibility'],
+        sac_scale = object.tags['sac_scale'],
+        tracktype = object.tags['tracktype'],
+        highway = object.tags.highway,
+        name = object.tags.name,
+        ref = object.tags.ref,
+        access = object.tags.access,
+        incline = object.tags.incline,
+        surface = object.tags.surface,
+        ford = object.tags.ford,
+        tags = object.tags,
+        geom = object:as_linestring(),
+    }
+
+    local d = w2r[object.id]
+    if d then
+        local refs = {}
+        local ids = {}
+        for rel_id, rel_ref in pairs(d) do
+            refs[#refs + 1] = rel_ref
+            ids[#ids + 1] = rel_id
+        end
+        table_sort(refs)
+        table_sort(ids)
+        a.rel_refs = table_concat(refs, ',')
+        a.rel_ids = table_concat(ids, ',')
+    end
+    hiking_ways:insert(a)
+end
+
+   
+
+function osm2pgsql.process_relation(object)
+    if object.tags.type == 'route' and object.tags.route == 'hiking' then
+        local geom = object:as_multilinestring() --use multilinestring for relations
+        if geom then
+            process_hiking_route(object, geom) 
+        end
+    end
+end
+
