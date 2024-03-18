3349ae617f9ef73c681b58d8e0a6d9bdff9c5f80
gemanzo <gemanzo@users.noreply.github.com>
2024-03-18 09:30:28 +0000

    Check and fix styling

--- app/Models/HikingWay.php
+++ app/Models/HikingWay.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use App\Traits\OsmTagsProcessor;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class HikingWay extends Model
 {
--- app/Nova/Filters/ClassFilter.php
+++ app/Nova/Filters/ClassFilter.php
@@ -2,8 +2,8 @@
 
 namespace App\Nova\Filters;
 
-use Laravel\Nova\Filters\Filter;
 use Illuminate\Support\Facades\DB;
+use Laravel\Nova\Filters\Filter;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class ClassFilter extends Filter
@@ -18,7 +18,7 @@ class ClassFilter extends Filter
     /**
      * Apply the filter to the given query.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  NovaRequest  $request
      * @param  \Illuminate\Database\Eloquent\Builder  $query
      * @param  mixed  $value
      * @return \Illuminate\Database\Eloquent\Builder
@@ -31,7 +31,7 @@ class ClassFilter extends Filter
     /**
      * Get the filter's available options.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  NovaRequest  $request
      * @return array
      */
     public function options(NovaRequest $request)
@@ -39,6 +39,7 @@ class ClassFilter extends Filter
         //query the database in the places table to get all the classes value not repeating it and return it as an array
         $query = 'SELECT DISTINCT class FROM places WHERE class IS NOT NULL';
         $results = DB::select($query);
+
         return collect($results)->mapWithKeys(function ($item) {
             return [$item->class => $item->class];
         })->toArray();
