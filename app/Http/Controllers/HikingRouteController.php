a57f5052babeb4d2e30f54255b92638c5d323d3d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-26 11:44:42 +0100

    feat: implemented api for hiking routes and places

--- app/Http/Controllers/HikingRouteController.php
+++ app/Http/Controllers/HikingRouteController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HikingRoute;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HikingRouteController extends Controller
 {
-    //
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/features/hiking-routes/list",
+     *     operationId="listHikingRoutes",
+     *     tags={"HikingRoutes"},
+     *     summary="List all Hiking Routes",
+     *     description="Returns a list of Hiking Routes with their details",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/HikingRouteItem")
+     *         ),
+     *     ),
+     * )
+     */
+    public function list()
+    {
+        $hikingRoutes = HikingRoute::all(['osm_id', 'updated_at'])->mapWithKeys(function ($route) {
+            return [$route->osm_id => $route->updated_at->toIso8601String()];
+        });
+
+        return response()->json($hikingRoutes);
+    }
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/features/hiking-routes/{id}",
+     *     operationId="getHikingRouteById",
+     *     tags={"HikingRoutes"},
+     *     summary="Get Hiking Route by ID",
+     *     description="Returns a single Hiking Route in GeoJSON format",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Hiking Route ID",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/HikingRouteGeojsonFeature")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Hiking Route not found"
+     *     )
+     * )
+     */
+    public function show($id)
+    {
+        $hikingRoute = HikingRoute::find($id);
+
+        if ($hikingRoute === null) {
+            return response()->json(['error' => 'Hiking Route not found'], 404);
+        }
+        $geom = DB::select('SELECT ST_AsGeoJSON(?) AS geojson', [$hikingRoute->geom])[0]->geojson;
+
+        $geojson = [
+            'type' => 'Feature',
+            'properties' => [
+                'name' => $hikingRoute->name ?? '',
+                'osm_id' => $hikingRoute->osm_id,
+                'osm_type' => $hikingRoute->osm_type,
+                'description' => $hikingRoute->description ?? '',
+                'tags' => $hikingRoute->tags ?? '{}',
+
+            ],
+            'geometry' => json_decode($geom, true),
+        ];
+
+        return response()->json($geojson);
+    }
 }
--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Place;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlaceController extends Controller
 {
-    //
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/features/places/list",
+     *     operationId="listPlaces",
+     *     tags={"Places"},
+     *     summary="List all Places",
+     *     description="Returns a list of Places with their details",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/PlaceItem")
+     *         ),
+     *     ),
+     * )
+     */
+    public function list()
+    {
+        $places = Place::all(['osm_id', 'updated_at'])->mapWithKeys(function ($place) {
+            return [$place->osm_id => $place->updated_at->toIso8601String()];
+        });
+
+        return response()->json($places);
+    }
+
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/features/places/{id}",
+     *     operationId="getPlaceById",
+     *     tags={"Places"},
+     *     summary="Get Place by ID",
+     *     description="Returns a single Place in GeoJSON format",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Place ID",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/GeoJsonFeature")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Place not found"
+     *     )
+     * )
+     */
+    public function show($id)
+    {
+        $place = Place::where('osm_id', $id)->first();
+
+        if (!$place) {
+            return response()->json(['message' => 'place non trovato'], 404);
+        }
+        $geom = DB::select('SELECT ST_AsGeoJSON(?) AS geojson', [$place->geom])[0]->geojson;
+        $geojsonFeature = [
+            'type' => 'Feature',
+            'properties' => [
+                'name' => $place->name,
+                'class' => $place->class,
+                'subclass' => $place->subclass,
+                'osm_id' => $place->osm_id,
+                'osm_type' => $place->osm_type,
+                'tags' => $place->tags,
+            ],
+            'geometry' => json_decode($geom, true),
+        ];
+
+        return response()->json($geojsonFeature);
+    }
 }
