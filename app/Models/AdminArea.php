ee226cf3411646434a134bf1f0f58ab5e2f68b5b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-04-04 11:04:14 +0200

    refactor: changed primary key to internal id for models

--- app/Models/AdminArea.php
+++ app/Models/AdminArea.php
@@ -12,7 +12,7 @@ class AdminArea extends Pivot
 
     protected $table = 'admin_areas';
 
-    protected $primaryKey = 'osm_id'; //set the primary key to osm_id because we do not have an id column
+    protected $primaryKey = 'id'; //set the primary key to osm_id because we do not have an id column
 
     protected $fillable = [
         'osm_id',
--- app/Models/HikingRoute.php
+++ app/Models/HikingRoute.php
@@ -12,7 +12,7 @@ class HikingRoute extends Model
 
     protected $table = 'hiking_routes';
 
-    protected $primaryKey = 'osm_id';
+    protected $primaryKey = 'id';
 
     protected $fillable = [
         'updated_at',
