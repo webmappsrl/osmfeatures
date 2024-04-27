ff721bc452ab4568d6413c37b773af9e7b5fac23
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-04-27 13:52:36 +0200

    feat: updated readme

--- app/Console/Commands/HikingRoutesCorrectTimestamp.php
+++ app/Console/Commands/HikingRoutesCorrectTimestamp.php
@@ -31,8 +31,22 @@ class HikingRoutesCorrectTimestamp extends Command
     {
         try {
             // Get the most recent last update from osm2pgsql_last_updates table in the imported_at column
-            $lastUpdate = DB::select('SELECT MAX(imported_at) as imported_at FROM osm2pgsql_last_updates')[0]->imported_at;
-            $lastUpdate ? $this->info('Last update: ' . $lastUpdate) : $this->info('No last update found');
+            $lastUpdate = DB::table('osm2pgsql_crontab_updates')
+                ->select(DB::raw('MAX(imported_at) as imported_at'))
+                ->where('from_lua', '=', 'hiking_routes.lua')
+                ->get();
+            $lastUpdate = $lastUpdate[0]->imported_at;
+
+            //subtract one day to the last update to avoid missing ways
+
+            if ($lastUpdate != null) {
+                //subtract one day to the last update
+                $lastUpdate = Carbon::parse($lastUpdate[0]->imported_at)->subDay();
+                $this->info('Last update: ' . $lastUpdate);
+            } else {
+                $this->info('No last update found');
+            }
+
             $this->info('Selecting hiking routes ways updated after the last update...');
 
             // Query the database to get all the hiking_routes_ways that have the updated_at value that is more recent than the last update date
@@ -51,7 +65,7 @@ class HikingRoutesCorrectTimestamp extends Command
 
             // Dispatch a job for each hiking_routes_ways
             foreach ($hikingRoutesWays as $hikingRoutesWay) {
-                //check if the way is a member of a hiking route
+                //only dispatch jobs for ways that are part of a hiking route
                 $hikingRoutes = DB::table('hiking_routes')->whereJsonContains('members', [['type' => 'w', 'ref' => $hikingRoutesWay->osm_id]])->get();
                 if ($hikingRoutes->count() > 0) {
                     dispatch(new ProcessHikingRoutesWayJob($hikingRoutesWay));
@@ -64,10 +78,6 @@ class HikingRoutesCorrectTimestamp extends Command
             $bar->finish();
             $this->info(''); // Add a new line after the progress bar (for better readability
             $this->info('Jobs dispatched successfully!');
-
-            // Update the imported_at column in the osm2pgsql_last_updates table
-            DB::table('osm2pgsql_last_updates')
-                ->insert(['imported_at' => Carbon::now()]);
         } catch (\Exception $e) {
             $this->error('Error in HikingRoutesCorrectTimestamp command: ' . $e->getMessage());
             Log::error('Error in HikingRoutesCorrectTimestamp command: ' . $e->getMessage());
