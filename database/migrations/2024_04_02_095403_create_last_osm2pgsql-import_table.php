64f33296018c06d8ab719ed62a758fe54c5e959a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-04-27 16:16:09 +0200

    fix: fixed migration

--- database/migrations/2024_04_02_095403_create_last_osm2pgsql-import_table.php
+++ database/migrations/2024_04_02_095403_create_last_osm2pgsql-import_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -25,6 +26,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('last_osm2pgsql_import');
+        Schema::dropIfExists('osm2pgsql_crontab_updates');
     }
 };
