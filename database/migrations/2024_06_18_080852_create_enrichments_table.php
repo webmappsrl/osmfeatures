769c4566c5ea517166ff80752128f5760b3ab95c
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-06-19 11:00:50 +0200

    fix: added timestamps to enrichment migration

--- database/migrations/2024_06_18_080852_create_enrichments_table.php
+++ database/migrations/2024_06_18_080852_create_enrichments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->morphs('enrichable');
             $table->json('data')->nullable();
+            $table->timestamps();
         });
     }
 
