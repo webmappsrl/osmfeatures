59a3bd3d9d7fa7393283c5aca873685e9ac1e430
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-06-24 08:42:21 +0200

    feat: enriched data added to places api.

--- app/Console/Commands/EnrichmentCommand.php
+++ app/Console/Commands/EnrichmentCommand.php
@@ -8,20 +8,37 @@ use App\Models\Place;
 
 class EnrichmentCommand extends Command
 {
-    protected $signature = 'osmfeatures:enrich';
+    protected $signature = 'osmfeatures:enrich {osmid?*}';
 
-    protected $description = 'Enrich osmfeatures places with data from Wikipedia, Wikidata, and Wikimedia Commons';
+    protected $description = 'Enrich osmfeatures places with data from Wikipedia, Wikidata, and Wikimedia Commons. Optionally provide a list of OSM IDs separated by space.';
 
     public function handle()
     {
+        $osmids = $this->argument('osmid');
         $jobCount = 0;
 
-        Place::chunk(100, function ($places) use (&$jobCount) {
-            foreach ($places as $place) {
-                EnrichmentJob::dispatch($place);
-                $jobCount++;
+        if (!empty($osmids)) {
+            $places = Place::whereIn('osm_id', $osmids);
+
+            if ($places->count() == 0) {
+                $this->info('No places found with the specified OSM IDs.');
+                return Command::FAILURE;
             }
-        });
+
+            $places->chunk(100, function ($places) use (&$jobCount) {
+                foreach ($places as $place) {
+                    EnrichmentJob::dispatch($place);
+                    $jobCount++;
+                }
+            });
+        } else {
+            Place::chunk(100, function ($places) use (&$jobCount) {
+                foreach ($places as $place) {
+                    EnrichmentJob::dispatch($place);
+                    $jobCount++;
+                }
+            });
+        }
 
         $this->info('Enrichment jobs dispatched successfully.');
         $this->info("Total jobs dispatched: $jobCount");
