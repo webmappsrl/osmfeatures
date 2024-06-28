31b1e83ab1de6d60da6a08bc7e208f88f0d4ef72
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-06-28 11:35:37 +0200

    chore: refactored enrichment command

--- app/Console/Commands/EnrichmentCommand.php
+++ app/Console/Commands/EnrichmentCommand.php
@@ -5,34 +5,38 @@ namespace App\Console\Commands;
 use App\Jobs\EnrichmentJob;
 use Illuminate\Console\Command;
 use App\Models\Place;
+use Illuminate\Support\Facades\App;
 
 class EnrichmentCommand extends Command
 {
-    protected $signature = 'osmfeatures:enrich {osmid?*}';
+    protected $signature = 'osmfeatures:enrich {model=Place: The model to enrich}{osmid?* : List of OSM IDs to enrich}';
 
-    protected $description = 'Enrich osmfeatures places with data from Wikipedia, Wikidata, and Wikimedia Commons. Optionally provide a list of OSM IDs separated by space.';
+    protected $description = 'Enrich provided osmfeatures model with data from Wikipedia, Wikidata, and Wikimedia Commons. Optionally provide a list of OSM IDs separated by space.';
+
+    protected $usage = 'osmfeatures:enrich {model=Place: The model to enrich}{osmid?* : List of OSM IDs to enrich}';
 
     public function handle()
     {
         try {
             $osmids = $this->argument('osmid');
             $jobCount = 0;
+            $model = App::make('App\\Models\\' . $this->argument('model'));
 
 
             if (!empty($osmids)) {
-                $places = Place::whereIn('osm_id', $osmids);
+                $models = $model::whereIn('osm_id', $osmids);
 
-                if ($places->count() == 0) {
-                    $this->info('No places found with the specified OSM IDs.');
+                if ($models->count() == 0) {
+                    $this->info('No models found with the specified OSM IDs.');
                     return Command::FAILURE;
                 }
             } else {
-                $places = Place::all();
+                $models = $model::all();
             }
 
-            $places->chunk(100, function ($places) use (&$jobCount) {
-                foreach ($places as $place) {
-                    EnrichmentJob::dispatch($place);
+            $models->chunk(100, function ($models) use (&$jobCount) {
+                foreach ($models as $model) {
+                    EnrichmentJob::dispatch($model);
                     $jobCount++;
                 }
             });
