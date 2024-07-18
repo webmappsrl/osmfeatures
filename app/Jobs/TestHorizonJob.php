8760e48bbde3cac9083c28f1f858b1fc633824bc
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-07-18 13:22:49 +0200

    chore: created laravel horizon test jobs

---
+++ app/Jobs/TestHorizonJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TestHorizonJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        Log::info('TestJob is executed');
+
+        Redis::set('test_key', 'test_value');
+        $value = Redis::get('test_key');
+        Log::info('Redis value: ' . $value);
+    }
+}
