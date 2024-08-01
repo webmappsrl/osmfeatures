a55ae99dc93591c9e11e07270cef33842f82085b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-01 11:09:38 +0200

    chore: new docker horizon and supervisor configuration

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-set -e
 
-source ./.env.example
+echo "Production deployment started ..."
 
-# echo "Production deployment started ..."
+php artisan down
 
-# php artisan down
+composer install
+composer dump-autoload
 
-# composer install
-# composer dump-autoload
+# Clear and cache config
+php artisan config:cache
+php artisan config:clear
 
-# # Clear and cache config
-# php artisan config:cache
-# php artisan config:clear
+# Clear the old cache
+php artisan clear-compiled
+php artisan optimize
 
-# # Clear the old cache
-# php artisan clear-compiled
+ php artisan migrate --force
 
-# # TODO: Uncomment when api.favorite issue will be resolved
-# # php artisan optimize
+# gracefully terminate laravel horizon
+php artisan horizon:terminate
 
-# php artisan migrate --force
-
-# gracefully terminate laravel horizon in supervisor container
-docker exec -it supervisor_$APP_NAME bash -c "cd /var/www/html/$APP_NAME && php artisan horizon:terminate"
-
-#  php artisan up
+php artisan up
 
 echo "Deployment finished!"
