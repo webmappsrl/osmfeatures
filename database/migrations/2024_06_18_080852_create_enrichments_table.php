2a8a1318fa3f7897d81330249e723b06bea3bb38
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-01 11:28:53 +0200

    fix

--- database/migrations/2024_06_18_080852_create_enrichments_table.php
+++ database/migrations/2024_06_18_080852_create_enrichments_table.php
@@ -4,18 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up()
     {
-        Schema::create('enrichments', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('enrichable');
-            $table->json('data')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('enrichments')) {
+            Schema::create('enrichments', function (Blueprint $table) {
+                $table->id();
+                $table->morphs('enrichable');
+                $table->json('data')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
--- database/migrations/2024_06_27_124853_add_enrichable_osmfeatures_id_to_enrichments_table.php
+++ database/migrations/2024_06_27_124853_add_enrichable_osmfeatures_id_to_enrichments_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('enrichments', function (Blueprint $table) {
-            $table->string('enrichable_osmfeatures_id')->nullable();
-        });
+        if (!Schema::hasColumn('enrichments', 'enrichable_osmfeatures_id')) {
+            Schema::table('enrichments', function (Blueprint $table) {
+                $table->string('enrichable_osmfeatures_id')->nullable();
+            });
+        }
     }
 
     /**
