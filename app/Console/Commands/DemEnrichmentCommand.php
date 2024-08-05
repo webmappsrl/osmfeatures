db04db54d48b698fdb1b5503208c50d20012abc5
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-05 16:12:24 +0200

    feat: added dem enrichments command

---
+++ app/Console/Commands/DemEnrichmentCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\DemEnrichmentJob;
+use Illuminate\Console\Command;
+
+class DemEnrichmentCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'osmfeatures:dem-enrichment {model : The model class} {id? : The Osmfeatures ID}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Launch dem enrichments jobs for the given model and Osmfeatures ID. If no ID is provided, all Osmfeatures will be processed.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $osmfeaturesId = $this->argument('id');
+        $model = $this->argument('model');
+
+        if ($osmfeaturesId) {
+            $modelClass = 'App\\Models\\' . $model;
+
+            if (!class_exists($modelClass)) {
+                $this->error("The model class $modelClass does not exist.");
+                return;
+            }
+
+            $relatedModel = $modelClass::getOsmfeaturesByOsmfeaturesID($osmfeaturesId);
+            DemEnrichmentJob::dispatch($relatedModel);
+        } else {
+            $relatedModels = $model::all();
+            foreach ($relatedModels as $model) {
+                DemEnrichmentJob::dispatch($model);
+            }
+        }
+    }
+}
