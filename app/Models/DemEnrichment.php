1665216cad884f952c6b98b5e11225b9b4f46271
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-05 16:11:57 +0200

    feat: added dem enrichment model

---
+++ app/Models/DemEnrichment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DemEnrichment extends Enrichment
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'data',
+        'dem-enrichable_id',
+        'dem-enrichable_type',
+        'enrichable_osmfeatures_id',
+    ];
+
+    public function demEnrichable()
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2024_08_05_125704_create_dem_enrichments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dem_enrichments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->morphs('dem-enrichable');
+            $table->jsonb('data')->nullable();
+            $table->string('enrichable_osmfeatures_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dem_enrichments');
+    }
+};
