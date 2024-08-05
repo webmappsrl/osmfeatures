8884b8dff1c4acb39825c9e60c80092bd25940e0
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-05 16:12:11 +0200

    chore: extended enrichable trait

--- app/Traits/Enrichable.php
+++ app/Traits/Enrichable.php
@@ -3,11 +3,17 @@
 namespace App\Traits;
 
 use App\Models\Enrichment;
+use App\Models\DemEnrichment;
 
 trait Enrichable
 {
     public function enrichment()
     {
         return $this->morphOne(Enrichment::class, 'enrichable');
+    }
+
+    public function demEnrichment()
+    {
+        return $this->morphOne(DemEnrichment::class, 'dem-enrichable');
     }
 }
