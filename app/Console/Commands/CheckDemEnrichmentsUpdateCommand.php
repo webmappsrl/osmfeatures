d1bd091a7845f3638d513722d68a07cc6d398672
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-08-08 08:51:49 +0200

    fix on check dem enrichments update command

--- app/Console/Commands/CheckDemEnrichmentsUpdateCommand.php
+++ app/Console/Commands/CheckDemEnrichmentsUpdateCommand.php
@@ -14,14 +14,14 @@ class CheckDemEnrichmentsUpdateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'osmfeatures:check-dem-enrichments-update';
+    protected $signature = 'osmfeatures:check-dem-enrichments-update {model : The name of the model} {id? : The osmfeatures ID of the model}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This command loop over all dem enrichments record in database and compare them to the related model updated_at value. If the model updated_at value is greater than the dem enrichments record updated_at value, the dem enrichments record will be updated making another call to dem api.';
+    protected $description = 'This command loop over all dem enrichments record in database and compare them to the related model updated_at value (if osmfeatures id not provided). If the model updated_at value is greater than the dem enrichments record updated_at value, the dem enrichments record will be updated making another call to dem api.';
 
     /**
      * Execute the console command.
@@ -29,14 +29,25 @@ class CheckDemEnrichmentsUpdateCommand extends Command
     public function handle()
     {
         $logger = Log::channel('dem-enrichment');
+        $modelClass = 'App\\Models\\' . $this->argument('model');
+        if (!class_exists($modelClass)) {
+            $this->error("The model class $modelClass does not exist.");
+            $logger->error("The model class $modelClass does not exist.");
+            return;
+        }
+        if ($this->argument('id')) {
+            $demEnrichments = collect(DemEnrichment::where('enrichable_osmfeatures_id', $this->argument('id'))->get());
+        } else {
+            $demEnrichments = DemEnrichment::all();
+        }
 
         $logger->info('Checking dem enrichments update...');
-        $demEnrichments = DemEnrichment::all();
         $progressBar = $this->output->createProgressBar(count($demEnrichments));
         foreach ($demEnrichments as $enrichment) {
             $progressBar->advance();
             $enrichmentTimestamp = $enrichment->updated_at;
-            $modelTimestamp = $enrichment->demEnrichable->updated_at;
+            $modelInstance = $modelClass::getOsmfeaturesByOsmfeaturesID($enrichment->enrichable_osmfeatures_id);
+            $modelTimestamp = $modelInstance->updated_at;
             //if the enrichment is outdated perform a dem enrichment job on the related model
             if ($modelTimestamp > $enrichmentTimestamp) {
                 $logger->info('Enrichment ' . $enrichment->id . ' is outdated. Dispatching job for Enrich model ' . get_class($enrichment->demEnrichable) . ' ' . $enrichment->demEnrichable->osm_type . $enrichment->demEnrichable->osm_id);
